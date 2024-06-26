eb90d06ce312717086a49d27b46f01a2faabbc66
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-06-27 08:34:48 +0900

    feat: Interact Object

---
+++ Assets/01.Scripts/Objects/FieldObject/Interaction/JunkFileObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ObjectManage;
+using UnityEngine;
+
+public class JunkFileObject : InteractObject
+{
+    private Rigidbody2D _rigidCompo;
+    [SerializeField] private float _pushPower;
+    private void Awake()
+    {
+        _rigidCompo = GetComponent<Rigidbody2D>();
+
+        OnInteractEvent += HandlePush;
+    }
+
+    private void HandlePush()
+    {
+        _rigidCompo.AddForce();
+    }
+}
