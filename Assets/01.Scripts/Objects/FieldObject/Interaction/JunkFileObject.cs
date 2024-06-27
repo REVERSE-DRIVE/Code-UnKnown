5e922a9cadacb0baa744ec6883e5c7e04768ee08
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-06-27 23:11:20 +0900

    feat: JunkPush

--- Assets/01.Scripts/Objects/FieldObject/Interaction/JunkFileObject.cs
+++ Assets/01.Scripts/Objects/FieldObject/Interaction/JunkFileObject.cs
@@ -7,12 +7,33 @@ public class JunkFileObject : InteractObject
     private Rigidbody2D _rigidCompo;
     [SerializeField] private float _pushPower;
     private Vector2 _origin;
+    [SerializeField] private int _damage = 3;
+    private bool _isActive;
     
     private void Awake()
     {
         _rigidCompo = GetComponent<Rigidbody2D>();
 
         OnInteractEvent += HandlePush;
+    }
+
+    private void Update()
+    {
+        if (!_isActive)
+            return;
+            
+        float magnitude = _rigidCompo.velocity.magnitude;
+        if (magnitude <= 0.1f)
+        {
+            _isActive = false;
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.TryGetComponent(out Health health))
+            HandleCollisionEvent(health);
     }
 
     public override void Interact(InteractData data)
@@ -26,4 +47,11 @@ public class JunkFileObject : InteractObject
         Vector2 direction = (Vector2)transform.position - _origin;
         _rigidCompo.AddForce(direction.normalized * _pushPower, ForceMode2D.Impulse);
     }
+
+    private void HandleCollisionEvent(Health hit)
+    {
+        hit.TakeDamage(_damage);
+    }
+    
+    
 }
