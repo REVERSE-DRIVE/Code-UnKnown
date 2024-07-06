231924946cd5fd4f7be7c8b62f9d684688babf37
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-06 20:38:43 +0900

    feat: Gear Attack

--- Assets/01.Scripts/Combat/Projectile/Gear.cs
+++ Assets/01.Scripts/Combat/Projectile/Gear.cs
@@ -1,13 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Gear : MonoBehaviour
 {
     [SerializeField] private Transform _centerTrm;
     [SerializeField] private float _speed;
     [SerializeField] private bool _isRotate;
-    [SerializeField] private float _radius = 1f; // 원운동의 반지름
-    
-    private float _angle; // 현재 각도
+    [SerializeField] private float _radius = 1f;
+    [SerializeField] private int _damage = 10;
+    private float _angle;
     
     public void SetRotate(bool value, float speed, float radius, float initialAngle)
     {
@@ -26,6 +27,14 @@ public class Gear : MonoBehaviour
             float x = _centerTrm.position.x + Mathf.Cos(radians) * _radius;
             float y = _centerTrm.position.y + Mathf.Sin(radians) * _radius;
             transform.position = new Vector2(x, y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out Health health))
+        {
+            health.TakeDamage(_damage);
         }
     }
 }
