29bc8f9cc111cc3a8801cb0da8666287f6a1b372
qazplm5602 <t01048187045@gmail.com>
2024-07-10 01:04:21 +0900

    feat: get possible pos

--- Assets/01.Scripts/Map/RoomBase.cs
+++ Assets/01.Scripts/Map/RoomBase.cs
@@ -37,6 +37,19 @@ public class RoomBase : MonoBehaviour
         };
     }
 
+    public virtual Vector2Int FindPossibleRandomPos(int spacing) {
+        Vector2Int pos = new(Random.Range(MinPos.x + 1 /* 테두리 */, MaxPos.x), Random.Range(MinPos.y + 1, MaxPos.y));
+        
+        Vector2Int min = pos - (Vector2Int.one * spacing);
+        Vector2Int max = pos + (Vector2Int.one * spacing);
+
+        if (MinPos.x > min.x || MinPos.y > min.y || MaxPos.x < max.x || MaxPos.y < max.y) {
+            return FindPossibleRandomPos(spacing); // 다시
+        }
+
+        return pos;
+    }
+
     public TileBase GetGroundTile() => groundTile;
 
     public virtual void RoomEnter() {
