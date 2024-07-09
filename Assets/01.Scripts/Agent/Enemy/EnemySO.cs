eb4c40eb6f6ab09620e5a11263126b3ca6b87648
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-09 13:59:04 +0900

    feat: EnemySpawnManager

---
+++ Assets/01.Scripts/Agent/Enemy/EnemySO.cs
@@ -0,0 +1,9 @@
+﻿using ObjectPooling;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/EnemySO", fileName = "EnemySO")]
+public class EnemySO : ScriptableObject
+{
+    public int id;
+    public PoolingType enemyType;
+}
