3f3bdcb8d109a1b93ad7728fe7c7141a60ec55e2
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-11 16:10:18 +0900

    feat: DoorTile

---
+++ Assets/01.Scripts/Combat/PlayerParts/Parts/GearPart.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerPartsManage
+{
+    public class GearPart : PlayerPart
+    {
+        public GearPart(Player owner) : base(owner)
+        {
+        }
+
+        public override void UseSkill()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
