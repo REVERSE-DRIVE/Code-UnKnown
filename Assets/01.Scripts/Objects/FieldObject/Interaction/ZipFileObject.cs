395e44ad81e367e7aa0a6da839df7d5296c15944
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-15 12:31:31 +0900

    feat: Yellow Zip File

--- Assets/01.Scripts/Objects/FieldObject/Interaction/ZipFileObject.cs
+++ Assets/01.Scripts/Objects/FieldObject/Interaction/ZipFileObject.cs
@@ -15,15 +15,20 @@ public class ZipFileObject : InteractObject
     [SerializeField] private Material _dissolveMaterial;
     
     private Transform _visualTransform;
+    
+    protected Material _defaultMaterial;
     protected Animator _animator;
     protected SpriteRenderer _visualRenderer;
+    
     private static readonly int Open = Animator.StringToHash("Open");
+    private static readonly int DissolveLevel = Shader.PropertyToID("_DissolveLevel");
 
     private void Awake()
     {
         _visualTransform = transform.Find("Visual");
         _animator = _visualTransform.GetComponent<Animator>();
         _visualRenderer = _visualTransform.GetComponent<SpriteRenderer>();
+        _defaultMaterial = _visualRenderer.material;
     }
 
     private void OnEnable()
@@ -38,7 +43,6 @@ public class ZipFileObject : InteractObject
 
     protected virtual void HandleInteract()
     {
-        
         Instantiate(_openParticle, transform.position, Quaternion.identity);
         StartCoroutine(Drop());
     }
@@ -47,9 +51,9 @@ public class ZipFileObject : InteractObject
     {
         _animator.SetTrigger(Open);
         yield return new WaitForSeconds(1.5f);
-        _visualRenderer.material = _dissolveMaterial;
-        _dissolveMaterial.DOFloat(0f, "_DissolveLevel", 0.5f);
+        Dissolve();
         yield return new WaitForSeconds(0.5f);
+        
         int dropAmount = Random.Range(_minDropAmount, _maxDropAmount);
         Vector3 position = transform.position;
         for (int i = 0; i < dropAmount; i++)
@@ -58,6 +62,19 @@ public class ZipFileObject : InteractObject
             ItemDropManager.Instance.DropItem(_dropType, 0, position);
             yield return new WaitForSeconds(0.5f);
         }
-        
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
+    private void Dissolve()
+    {
+        _visualRenderer.material = _dissolveMaterial;
+        _visualRenderer.material.DOFloat(0f, DissolveLevel, 0.5f);
+    }
+
+    private void Init()
+    {
+        _visualRenderer.material = _defaultMaterial;
+        _dissolveMaterial.SetFloat(DissolveLevel, 1f);
     }
 }
