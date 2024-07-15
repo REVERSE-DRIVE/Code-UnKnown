41951eb2dd83239773eae124c66d2ed5a2c5112d
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-15 12:18:33 +0900

    feat: ZipFile Open Animation

--- Assets/01.Scripts/Objects/FieldObject/Interaction/ZipFileObject.cs
+++ Assets/01.Scripts/Objects/FieldObject/Interaction/ZipFileObject.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using DG.Tweening;
 using ItemManage;
 using ObjectManage;
 using UnityEngine;
@@ -7,11 +8,24 @@ using Random = UnityEngine.Random;
 
 public class ZipFileObject : InteractObject
 {
-    [SerializeField] private int _resourceDropPercent;
-    [SerializeField] private int _weaponDropPercent;
+    [SerializeField] private ItemType _dropType;
     [SerializeField] private int _minDropAmount;
     [SerializeField] private int _maxDropAmount;
     [SerializeField] private ParticleSystem _openParticle;
+    [SerializeField] private Material _dissolveMaterial;
+    
+    private Transform _visualTransform;
+    protected Animator _animator;
+    protected SpriteRenderer _visualRenderer;
+    private static readonly int Open = Animator.StringToHash("Open");
+
+    private void Awake()
+    {
+        _visualTransform = transform.Find("Visual");
+        _animator = _visualTransform.GetComponent<Animator>();
+        _visualRenderer = _visualTransform.GetComponent<SpriteRenderer>();
+    }
+
     private void OnEnable()
     {
         OnInteractEvent += HandleInteract;
@@ -22,31 +36,26 @@ public class ZipFileObject : InteractObject
         OnInteractEvent -= HandleInteract;
     }
 
-    private void HandleInteract()
+    protected virtual void HandleInteract()
     {
+        
         Instantiate(_openParticle, transform.position, Quaternion.identity);
         StartCoroutine(Drop());
     }
 
     private IEnumerator Drop()
     {
+        _animator.SetTrigger(Open);
+        yield return new WaitForSeconds(1.5f);
+        _visualRenderer.material = _dissolveMaterial;
+        _dissolveMaterial.DOFloat(0f, "_DissolveLevel", 0.5f);
+        yield return new WaitForSeconds(0.5f);
         int dropAmount = Random.Range(_minDropAmount, _maxDropAmount);
-
+        Vector3 position = transform.position;
         for (int i = 0; i < dropAmount; i++)
         {
-            int random = Random.Range(0, 100);
-            ItemType type = ItemType.Resource;
-            if (random < _resourceDropPercent)
-            {
-                type = ItemType.Resource;
-            
-            }
-            else if (random < _resourceDropPercent + _weaponDropPercent)
-            {
-                type = ItemType.Weapon;
-            }
-            Vector3 position = transform.position + new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0);
-            ItemDropManager.Instance.DropItem(type, 0, position);
+            position += new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            ItemDropManager.Instance.DropItem(_dropType, 0, position);
             yield return new WaitForSeconds(0.5f);
         }
         
