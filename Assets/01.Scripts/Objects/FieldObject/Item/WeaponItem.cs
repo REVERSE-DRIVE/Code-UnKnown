768a8e3043eb3d879ede00753c8bc6ba0ccb2558
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-16 11:54:15 +0900

    feat: Weapon Item

--- Assets/01.Scripts/Objects/FieldObject/Item/WeaponItem.cs
+++ Assets/01.Scripts/Objects/FieldObject/Item/WeaponItem.cs
@@ -1,9 +1,19 @@
 ﻿using ItemManage;
+using UnityEngine;
+using WeaponManage;
 
 public class WeaponItem : Item
 {
+    [SerializeField] private WeaponInfoSO _weaponInfoSO;
+    public override void SetItem(ItemSO itemSO)
+    {
+        base.SetItem(itemSO);
+        _weaponInfoSO = itemSO.weaponInfoSO;
+    }
+
     public override void Interact(InteractData data)
     {
         base.Interact(data);
+        PlayerManager.Instance.player.PlayerAttackCompo.ChangeWeapon(_weaponInfoSO);
     }
 }
