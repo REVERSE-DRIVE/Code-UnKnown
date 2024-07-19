917ac30f0fac8046c463fab9b66d5cd6517917b3
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-19 21:46:51 +0900

    refactor: Utility Window

---
+++ Assets/01.Scripts/Combat/PlayerParts/PlayerBodyPartDataSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace PlayerPartsManage
+{
+    [CreateAssetMenu(menuName = "SO/PlayerParts/BodyPartData")]
+    public class PlayerBodyPartDataSO : PlayerPartDataSO
+    {
+        public Sprite bodyPartSprite;
+    }
+}
