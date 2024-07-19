89225feca4feb2153c5c6a66636e04ec02f9b87c
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-19 21:21:16 +0900

    feat: TextEffect

--- Assets/01.Scripts/Objects/EffectObject/TextEffectObject.cs
+++ Assets/01.Scripts/Objects/EffectObject/TextEffectObject.cs
@@ -1,63 +1,51 @@
-﻿using ObjectPooling;
+﻿using System.Collections;
+using DG.Tweening;
+using ObjectPooling;
 using TMPro;
 using UnityEngine;
 
 public struct TextContent
 {
     public string content;
+    public int size;
     public Color color;
     public float lifeTime;
 }
 
-public class TextEffectObject : MonoBehaviour, ILifeTimeLimited, IPoolable
+public class TextEffectObject : MonoBehaviour, IPoolable
 {
     [field:SerializeField] public PoolingType type { get; set; }
     public GameObject ObjectPrefab => gameObject;
+
+    [Header("TextEffect Setting")] 
+    [SerializeField] private float _yDelta;
     
+    [Header("Content Setting")]
     [SerializeField] private TextMeshPro _tmp;
     [SerializeField] private float _lifeTime;
-    [SerializeField]
-    private float _currentLifeTime;
     private bool _isActive;
-
-    float ILifeTimeLimited.CurrentLifeTime
-    {
-        get => _currentLifeTime;
-        set => _currentLifeTime = value;
-    }
-
-    private void Update()
-    {
-        _currentLifeTime += Time.deltaTime;
-    }
 
     public void Initialize(TextContent content)
     {
         _tmp.text = content.content;
         _tmp.color = content.color;
+        _tmp.fontSize = content.size;
         _lifeTime = content.lifeTime;
-        
-        
+        transform.localScale = Vector2.one;
     }
 
     public void Play()
     {
+        _isActive = true;
+        transform.DOScale(Vector2.zero, _lifeTime);
+        transform.DOMoveY(_yDelta, _lifeTime).OnComplete(()=> HandleDie());
     }
 
-    public void CheckDie()
-    {
-        if (_currentLifeTime > _lifeTime)
-        {
-            
-        }
-    }
-
-    public void HandleDie()
+    private void HandleDie()
     {
         PoolingManager.Instance.Push(this);
     }
 
-    
     public void ResetItem()
     {
         
--- Assets/01.Scripts/Utils/PoolManager/Core/ObjectPool/PoolingType.cs
+++ Assets/01.Scripts/Utils/PoolManager/Core/ObjectPool/PoolingType.cs
@@ -2,6 +2,6 @@ namespace ObjectPooling
 {
     public enum PoolingType
     {
-        Projectile_Tracing,EnemyLaser,Type_A,Type_A_Upgrade,ExplodeVFX,CriticalHitVFX,Projectile_Spining,ObjectBreakVFX,EnemyHitVFX,AttackVFX_1,WeaponBase,ResourceBase,
+        Projectile_Tracing,EnemyLaser,Type_A,Type_A_Upgrade,ExplodeVFX,CriticalHitVFX,Projectile_Spining,ObjectBreakVFX,EnemyHitVFX,AttackVFX_1,WeaponBase,ResourceBase,TextEffectObject,
     }
 }
