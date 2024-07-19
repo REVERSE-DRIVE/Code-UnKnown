a963b4cbdc6a97f9dc23b4e7fa2f063e03309f69
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-19 23:06:10 +0900

    feat: Resource Animation

--- Assets/01.Scripts/Objects/FieldObject/Item/ResourceItem.cs
+++ Assets/01.Scripts/Objects/FieldObject/Item/ResourceItem.cs
@@ -1,4 +1,6 @@
-﻿using ItemManage;
+﻿using System;
+using DG.Tweening;
+using ItemManage;
 using UnityEngine;
 
 public class ResourceItem : Item
@@ -9,6 +11,37 @@ public class ResourceItem : Item
     {
         base.SetItem(itemSO);
         _resourceRank = itemSO.resourceRank;
+    }
+
+    private void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        // 마인크래프트 exp 스타일로 플레이어를 따라다님
+        if (_isInteracted) return;
+        Vector3 targetPos = PlayerManager.Instance.player.transform.position;
+        float distance = (targetPos - transform.position).magnitude;
+        if (distance > 0.5f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, 0.1f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isInteracted = true;
+            Vector3 targetPos = PlayerManager.Instance.player.transform.position;
+            transform.DOJump(targetPos + new Vector3(0, 1, 0), 1, 1, 0.5f).OnComplete(() =>
+            {
+                LevelManager.Instance.ApplyExp(ItemSO.resourceValue);
+                gameObject.SetActive(false);
+            });
+        }
     }
 
     public override void Interact(InteractData data)
