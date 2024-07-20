86ef519f4e76b5e9f49b8803a7e0c1e13b4f2945
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-21 02:01:14 +0900

    fix: AddStat type

--- Assets/01.Scripts/Agent/StatSystem/AdditionalStat.cs
+++ Assets/01.Scripts/Agent/StatSystem/AdditionalStat.cs
@@ -10,7 +10,8 @@ public class AdditionalStat : ScriptableObject
     public Stat bonusResource;
     public Stat dashSpeed;
     public Stat attackRange; // 기본이 10이다
-    public Stat comboBonus;
+    public Stat comboBonusRate; // 기본 10, 1당 10%
+    
     public bool isStrongAttack;
     
     protected Agent _owner;
