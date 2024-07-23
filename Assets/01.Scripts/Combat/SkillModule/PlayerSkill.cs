e3bef33bc3a0b18b139b17bb7e9f63f078e00fc7
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-24 04:02:05 +0900

    feta: Skill Module

---
+++ Assets/01.Scripts/Combat/SkillModule/PlayerSkill.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CombatSkillManage
+{
+    public abstract class PlayerSkill : MonoBehaviour
+    {
+        protected Player _player;
+        
+        public void Initialize(Player player)
+        {
+            _player = player;
+        }
+
+        public abstract void UseSkill();
+    }
+}
---
+++ Assets/01.Scripts/Combat/SkillModule/PlayerSkillController.cs
@@ -0,0 +1,60 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CombatSkillManage
+{
+    
+    public class PlayerSkillController : MonoBehaviour
+    {
+        [SerializeField] private PlayerSkillSO _currentSkillSO;
+        [SerializeField] private Image _skillIconImage;
+        [SerializeField] private Image _skillCooltimeGaugeImage;
+        
+        private PlayerSkill _currentSkill;
+        private Player _player;
+
+        private float _currentTime;
+        [SerializeField] private bool _canUseSkill;
+        
+        public bool IsCoolDown => _currentTime >= _currentSkillSO.coolTime;
+        
+        private void Start()
+        {
+            _player = PlayerManager.Instance.player;
+            _player.PlayerInputCompo.controlButtons.OnSkillEvent += HandleUseSkill;
+        }
+
+        private void Update()
+        {
+            
+            _currentTime += Time.deltaTime;
+            Refresh();
+        }
+
+        public void Initialize()
+        {
+            _currentSkill = Instantiate(_currentSkillSO.skillPrefab, transform);
+            _currentSkill.Initialize(_player);
+            _skillIconImage.sprite = _currentSkillSO.skillIcon;
+            
+        }
+
+        public void HandleUseSkill()
+        {
+            if (!_canUseSkill) return;
+            if (!IsCoolDown) return;
+            _currentTime = 0;
+            _currentSkill.UseSkill();
+        }
+
+        private void Refresh()
+        {
+            _skillCooltimeGaugeImage.fillAmount = Mathf.Clamp01(_currentTime / _currentSkillSO.coolTime);
+        }
+
+
+    }
+
+}
