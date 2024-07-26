da99bcb891127ef26d2fa4433111a656faaf53f1
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-26 16:05:57 +0900

    feat: Split ComboCounter

---
+++ Assets/01.Scripts/Combat/PlayerCombat/PlayerComboCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerComboCounter : MonoBehaviour
+{
+    [Header("Combo Setting")] 
+    [SerializeField] private float _comboCancelTime;
+    private float _comboTime = 0;
+    public bool IsCombo => _comboTime < _comboCancelTime;
+    [field:SerializeField] public int comboCount { get; private set; } = 0;
+
+    private void Update()
+    {
+        _comboTime += Time.deltaTime;
+
+    }
+    
+    public void CountCombo()
+    {
+        if (IsCombo)
+        {
+            comboCount++;
+            
+        }
+        else
+            comboCount = 0;    
+        _comboTime = 0f;
+    }
+}
---
+++ Assets/01.Scripts/Combat/PlayerCombat/PlayerStrongAttacker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerStrongAttacker : MonoBehaviour
+{
+    private Player _player;
+    private PlayerComboCounter _comboCounter;
+
+    private void Awake()
+    {
+        _player = GetComponent<Player>();
+        _comboCounter = GetComponent<PlayerComboCounter>();
+    }
+
+    private void Start()
+    {
+        _player.PlayerInputCompo.controlButtons.actionButton.OnHoldEvent += HandleStrongAttack;
+    }
+
+    public void HandleStrongAttack()
+    {
+        
+    }
+}
