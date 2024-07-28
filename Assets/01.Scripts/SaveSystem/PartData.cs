f9a29a0fc20ff11b8a96a34ca9e75b3695b004c1
leo82380 <tkdrb062@gmail.com>
2024-07-28 18:39:49 +0900

    feat: InGameSaveData and SaveManager

---
+++ Assets/01.Scripts/SaveSystem/PartData.cs
@@ -0,0 +1,10 @@
+﻿using PlayerPartsManage;
+
+[System.Serializable]
+public struct PartData
+{
+    public int partID;
+    public PartType partType;
+    public int partLevel;
+    public int partRemainingPeriod;
+}
