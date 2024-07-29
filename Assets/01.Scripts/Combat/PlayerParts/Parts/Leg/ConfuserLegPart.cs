94ae2e386c9107a17edbe9d3128840fad5b1dfe8
leo82380 <tkdrb062@gmail.com>
2024-07-29 19:59:43 +0900

    feat: PlayerLegPart

---
+++ Assets/01.Scripts/Combat/PlayerParts/Parts/Leg/ConfuserLegPart.cs
@@ -0,0 +1,25 @@
+﻿using PlayerPartsManage;
+using UnityEngine;
+
+public class ConfuserLegPart : PlayerPart
+{
+    private int _attackPower = 0;
+    private int dashSpeed = 0;
+    public ConfuserLegPart(Player owner) : base(owner)
+    {
+    }
+
+    public override void UseSkill()
+    {
+        _owner.PlayerInputCompo.OnMovementEvent += Confuse;
+        _attackPower = _owner.Stat.GetDamage();
+        _owner.Stat.damage.AddModifier(_attackPower * 20 / 100);
+        //dashSpeed = _owner.DashSpeed;
+        //_owner.DashSpeed += DashSpeed * 20 / 100;
+    }
+
+    private void Confuse(Vector2 moveVector)
+    {
+        moveVector = new Vector2(-moveVector.x, -moveVector.y);
+    }
+}
--- Assets/01.Scripts/Combat/PlayerParts/Parts/Leg/StackerLegPart.cs
+++ Assets/01.Scripts/Combat/PlayerParts/Parts/Leg/StackerLegPart.cs
@@ -1,14 +1,36 @@
-﻿using PlayerPartsManage;
+﻿using System.Collections;
+using PlayerPartsManage;
 
 public class StackerLegPart : PlayerPart
 {
-        
+    private int stackCount = 0;
     public StackerLegPart(Player owner) : base(owner)
     {
     }
 
     public override void UseSkill()
     {
-        
+        StartCoroutine(StackCount());
+    }
+
+    private IEnumerator StackCount()
+    {
+        while (true)
+        {
+            if (_owner.PlayerController.Velocity.magnitude > 0)
+            {
+                //if (_owner.IsDash)
+                {
+                    if (stackCount >= 10)
+                    {
+                        // 3초간 적 기절
+                        // enemy 20% 추가 피해
+                        stackCount = 0;
+                    }
+                    stackCount++;
+                }
+            }
+            yield return null;
+        }
     }
 }
