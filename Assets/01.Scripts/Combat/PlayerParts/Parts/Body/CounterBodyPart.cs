3c1dcad080f54ed9cf47c6eaebc72fa06c08bce5
leo82380 <tkdrb062@gmail.com>
2024-07-31 09:14:10 +0900

    feat: Counter Burst

--- Assets/01.Scripts/Combat/PlayerParts/Parts/Body/CounterBodyPart.cs
+++ Assets/01.Scripts/Combat/PlayerParts/Parts/Body/CounterBodyPart.cs
@@ -1,10 +1,12 @@
-﻿using PlayerPartsManage;
+﻿using DG.Tweening;
+using PlayerPartsManage;
 using UnityEngine;
 
 public class CounterBodyPart : PlayerPart
 {
     [SerializeField] private LayerMask _whatIsEnemy;
     [SerializeField] private ParticleSystem _explosion;
+    [SerializeField] private Transform _shockWave;
     private int count = 0;
     private Collider2D[] _colliders;
     public CounterBodyPart(Player owner) : base(owner)
@@ -36,6 +38,10 @@ public class CounterBodyPart : PlayerPart
         if (count >= 3)
         {
             Instantiate(_explosion, _owner.transform.position, Quaternion.identity);
+            _shockWave.DOPunchScale(new Vector3(2f, 2f, 1), 0.5f, 1).OnComplete(() =>
+            {
+                _shockWave.transform.localScale = Vector3.zero;
+            });
             CameraManager.Instance.Shake(5, 0.5f);
             int enemyCount = 
                 Physics2D.OverlapBoxNonAlloc(_owner.transform.position, new Vector2(3, 3), 0, _colliders, _whatIsEnemy);
