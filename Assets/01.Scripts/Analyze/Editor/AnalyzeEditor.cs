f97d8279d8d021e6e8b26d7dfa0f9f4bf0f17f7f
qazplm5602 <t01048187045@gmail.com>
2024-08-01 12:14:38 +0900

    feat: auto analyze init

---
+++ Assets/01.Scripts/Analyze/Editor/AnalyzeEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[InitializeOnLoad]
+public static class AnalyzeEditor
+{
+    static AnalyzeEditor()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange status)
+    {
+        if (status == PlayModeStateChange.EnteredPlayMode)
+        {
+            AnalyzeSingleton core = GameObject.FindAnyObjectByType<AnalyzeSingleton>();
+            if (core != null) return;
+
+            GameObject entity = new GameObject("[AnalyzeCore]");
+            entity.AddComponent<AnalyzeSingleton>();
+            entity.AddComponent<AnalyzePing>();
+            entity.AddComponent<AnalyzeScene>();
+        }
+    }
+}
