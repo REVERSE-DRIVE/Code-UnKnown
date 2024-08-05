b7aeea6fc2b286cec114e70e84d9d8faf946515d
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-08-06 00:56:35 +0900

    feat: Attack Trigger Skill, Defender Skill

---
+++ Assets/01.Scripts/SkillSystem/AttackTriggerSkill.cs
@@ -0,0 +1,14 @@
+﻿public abstract class AttackTriggerSkill : Skill
+{
+    protected int _attackCount;
+    protected override void Start()
+    {
+        base.Start();
+        player.PlayerAttackCompo.OnAttackEvent += HandlePlayerAttackEvent;
+    }
+
+    protected virtual void HandlePlayerAttackEvent()
+    {
+        _attackCount++;
+    }
+}
