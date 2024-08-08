8a9589f96b470e91f985a9de53012133706605e9
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-08-09 01:17:12 +0900

    refactor: EffectObject Structure

---
+++ Assets/01.Scripts/Objects/EffectObject/ExplodeMarkEffectObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine;
+
+namespace ObjectManage
+{
+    public class ExplodeMarkEffectObject: EffectObject
+    {
+        private Transform _visualTrm;
+        private SpriteRenderer _spriteRenderer;
+        private Material _material;
+        private int _dissolveHash;
+        private int _noiseHash;
+        [SerializeField] private float _dissolveDuration = 1f;
+        [SerializeField] private Vector2 _noiseRandomRange;
+
+        private void Awake()
+        {
+            _dissolveHash = Shader.PropertyToID("_");
+            _visualTrm = transform.Find("Visual");
+            _visualTrm.GetComponent<SpriteRenderer>();
+        }
+
+
+        public override void Play()
+        {
+            StartCoroutine(EffectPlayCoroutine());
+
+        }
+
+        private IEnumerator EffectPlayCoroutine()
+        {
+            float currentTime = 0;
+            while (currentTime < _dissolveDuration)
+            {
+                currentTime += Time.deltaTime;
+                _material.SetFloat(_dissolveHash, Mathf.Lerp(1f, 0f, currentTime / _dissolveDuration));
+                yield return null;
+            }
+        }
+
+        public override void Stop()
+        {
+            
+        }
+
+        public override void ResetItem()
+        {
+        }
+    }
+}
--- Assets/01.Scripts/Objects/EffectObject/ParticleVFXObject.cs
+++ Assets/01.Scripts/Objects/EffectObject/ParticleVFXObject.cs
@@ -1,16 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
 
 namespace ObjectManage
 {
     public class ParticleVFXObject : EffectObject
     {
-        //[SerializeField] private ParticleSystem[] _particles;
+        [SerializeField] protected ParticleSystem[] _particles;
 
-        public void Play()
+        // 언젠가 이쪽으로 옮겨야함
+        public override void Play()
         {
             for (int i = 0; i < _particles.Length; i++)
             {
                 _particles[i].Play();
+            }
+        }
+
+        public override void Stop()
+        {
+            for (int i = 0; i < _particles.Length; i++)
+            {
+                _particles[i].Stop();
+            }
+        }
+
+        public override void ResetItem()
+        {
+            _currentLifeTime = 0;
+            if (_playOnSpawn)
+            {
+                _isActive = true;
+                Play();
             }
         }
     }
