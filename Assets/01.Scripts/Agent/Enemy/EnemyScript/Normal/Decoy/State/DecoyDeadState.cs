8728a20168c9ad2174422a903b55ae5832f36220
leo82380 <tkdrb062@gmail.com>
2024-08-09 13:49:35 +0900

    feat: Decoy Dead State Bomb

---
+++ Assets/01.Scripts/Agent/Enemy/EnemyScript/Normal/Decoy/State/DecoyDeadState.cs
@@ -0,0 +1,26 @@
+﻿using EnemyManage;
+using UnityEngine;
+
+public class DecoyDeadState : EnemyDeadState
+{
+    private float _radius = 4f;
+    private Collider2D[] _colliders = new Collider2D[10];
+    public DecoyDeadState(Enemy enemyBase, EnemyStateMachine<EnemyStateEnum> stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
+    {
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+        int count = Physics2D.OverlapCircleNonAlloc
+            (_enemyBase.transform.position, _radius, _colliders, _enemyBase.WhatIsPlayer);
+        for (int i = 0; i < count; i++)
+        {
+            if (_colliders[i].TryGetComponent(out Player player))
+            {
+                player.HealthCompo.TakeDamage(_enemyBase.Stat.GetDamage());
+            }
+            
+        }
+    }
+}
