5a671c77156d2d5d96f8b1023639376e75e26b7b
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-08-12 01:13:55 +0900

    feat: Font Setting

---
+++ Assets/01.Scripts/UI/TitleScene/CreditPanel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CreditPanel : MonoBehaviour, IWindowPanel
+{
+    
+    
+    public void Open()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Close()
+    {
+        throw new System.NotImplementedException();
+    }
+}
