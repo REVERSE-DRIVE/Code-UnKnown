af5ce8d8cd58095526d9243cc6f90c6edd4f14d9
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-08-12 22:58:56 +0900

    feat: Credit Show

--- Assets/01.Scripts/UI/TitleScene/CreditPanel.cs
+++ Assets/01.Scripts/UI/TitleScene/CreditPanel.cs
@@ -1,16 +1,26 @@
+using System;
 using UnityEngine;
 
 public class CreditPanel : MonoBehaviour, IWindowPanel
 {
-    
-    
+    [SerializeField] private RectTransform _credirHandle;
+    [SerializeField] private float _startYDelta;
+    [SerializeField] private float _endYDelta;
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
     public void Open()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public void Close()
     {
-        throw new System.NotImplementedException();
+        
     }
+    
 }
