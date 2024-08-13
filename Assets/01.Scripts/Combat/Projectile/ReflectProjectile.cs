62dfbf009909974fd9c1dbfb24ac2f1c93335bf7
leo82380 <tkdrb062@gmail.com>
2024-08-13 14:40:08 +0900

    fix: Resource Material

---
+++ Assets/01.Scripts/Combat/Projectile/ReflectProjectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ReflectProjectile : Projectile
+{
+    [SerializeField] private int _reflectCount;
+    
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (_reflectCount > 0)
+            {
+                _reflectCount--;
+                Vector2 reflectDir = Vector2.Reflect(_rigidCompo.velocity.normalized, other.transform.position - transform.position);
+                _rigidCompo.velocity = reflectDir * _speed;
+            }
+            else
+            {
+                base.OnTriggerEnter2D(other);
+            }
+        }
+        else
+        {
+            base.OnTriggerEnter2D(other);
+        }
+    }
+}
