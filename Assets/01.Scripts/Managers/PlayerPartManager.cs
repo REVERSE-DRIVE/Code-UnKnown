cbd79ea0bc415ad2639fb6cc45064e441230fede
leo82380 <tkdrb062@gmail.com>
2024-08-15 17:35:13 +0900

    feat: partSave

--- Assets/01.Scripts/Managers/PlayerPartManager.cs
+++ Assets/01.Scripts/Managers/PlayerPartManager.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using PlayerPartsManage;
+using SaveSystem;
 using UnityEngine;
 
 public class PlayerPartManager : MonoSingleton<PlayerPartManager>
 {
+    [SerializeField] private PlayerPartTableSO _partTable;
     [field:SerializeField] public List<PlayerPartDataSO> PlayerPartDataList { get; set; }
     [field:SerializeField] public PlayerBodyPartDataSO BodyPart { get; private set; }
     [field:SerializeField] public PlayerLegPartDataSO LegPart { get; private set; }
+    
+    private List<PartData> _partDataList;
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
+        _partDataList = new List<PartData>();
     }
 
     public void SetBodyPart(PlayerBodyPartDataSO bodyPart)
@@ -64,6 +69,52 @@ public class PlayerPartManager : MonoSingleton<PlayerPartManager>
     {
         if (PlayerPartDataList == null)
             PlayerPartDataList = new List<PlayerPartDataSO>();
+        
         PlayerPartDataList.Add(partData);
+        //AddData(partData);
+    }
+
+    private void AddData(PlayerPartDataSO partData)
+    {
+        _partDataList.Add(new PartData(partData.id, partData.partType));
+    }
+
+    public void SavePartData()
+    {
+        SaveManager.Instance.SaveToList(_partDataList, "PlayerPartData");
+    }
+    
+    public void LoadPartData()
+    {
+        _partDataList = SaveManager.Instance.LoadFromList<PartData>("PlayerPartData");
+        if (_partDataList == null)
+            return;
+
+        GivePart();
+    }
+
+    private void GivePart()
+    {
+        foreach (var partData in _partDataList)
+        {
+            var partDataSO = _partTable.playerPartDataSOList.Find
+                (x => x.id == partData.partID &&
+                      x.partType == partData.partType);
+            AddPartData(partDataSO);
+        }
+    }
+    
+    [ContextMenu("Test")]
+    public void Test()
+    {
+        AddData(PlayerPartDataList[0]);
+        AddData(PlayerPartDataList[1]);
+        SavePartData();
+    }
+    
+    [ContextMenu("LoadTest")]
+    public void LoadTest()
+    {
+        LoadPartData();
     }
 }
