1c46723ed29f251a93131aa1777ceb69250a1897
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-08-23 03:17:12 +0900

    feat: Fix Panel

--- Assets/01.Scripts/UI/GameScene/EditorPanel.cs
+++ Assets/01.Scripts/UI/GameScene/EditorPanel.cs
@@ -1,15 +1,26 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EditorPanel : MonoBehaviour, IWindowPanel
 {
     [SerializeField] private float _activeDuration;
+    [SerializeField] private Button _partBtn;
+    [SerializeField] private Button _fixBtn;
+    [SerializeField] private PartEditorPanel _partEditorPanel;
+    [SerializeField] private FixEditorPanel _fixEditorPanel;
+        
     private CanvasGroup _canvasGroup;
 
     private void Awake()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
+        _partBtn.onClick.AddListener(_fixEditorPanel.Close);
+        _partBtn.onClick.AddListener(_partEditorPanel.Open);
+
+        _fixBtn.onClick.AddListener(_partEditorPanel.Close);
+        _fixBtn.onClick.AddListener(_fixEditorPanel.Open);
     }
 
     public void Open()
--- Assets/01.Scripts/UI/GameScene/EditorPanel/FixEditorPanel.cs
+++ Assets/01.Scripts/UI/GameScene/EditorPanel/FixEditorPanel.cs
@@ -1,20 +1,49 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FixEditorPanel : MonoBehaviour, IWindowPanel
 {
+    [SerializeField] private Image _gaugeFill;
+    [SerializeField] private Image _gaugeSubFill;
+    [SerializeField] private float _openDuration = 0.1f;
+    [SerializeField] private float _gaugeFillDuration = 0.2f;
+    private Vector3 _defaultScale = Vector3.one;
+    private RectTransform _rectTrm;
     private CanvasGroup _canvasGroup;
 
     private void Awake()
     {
+        _rectTrm = transform as RectTransform;
+        
         _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void Open()
     {
         SetCanvas(true);
-
+        Sequence seq = DOTween.Sequence();
+        _rectTrm.localScale = Vector3.zero;
+        ResetGauge();
+        seq.SetUpdate(true);
+        seq.Append(_rectTrm.DOScaleX(_defaultScale.x, _openDuration));
+        seq.Append(_rectTrm.DOScaleY(_defaultScale.y, _openDuration));
+        seq.AppendCallback(RefreshGauge);
     }
 
+    private void RefreshGauge()
+    {
+        ResetGauge();
+        _gaugeSubFill.DOFillAmount(0.8f, _gaugeFillDuration);
+        _gaugeFill.DOFillAmount(0.5f, _gaugeFillDuration);
+    }
+
+    private void ResetGauge()
+    {
+        _gaugeSubFill.fillAmount = 0f;
+        _gaugeFill.fillAmount = 0f;
+    }
+    
     public void Close()
     {
         SetCanvas(false);
