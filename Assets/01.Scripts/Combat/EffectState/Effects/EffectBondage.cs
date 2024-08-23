672d95eb6d9d2f38f7a1632793516f6ca0d8481e
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-08-23 22:06:02 +0900

    feat: Skill VFX

--- Assets/01.Scripts/Combat/EffectState/Effects/EffectBondage.cs
+++ Assets/01.Scripts/Combat/EffectState/Effects/EffectBondage.cs
@@ -1,8 +1,12 @@
-﻿namespace EffectState
+﻿using ObjectManage;
+using ObjectPooling;
+
+namespace EffectState
 {
     public class EffectBondage : EffectState
     {
         private int _buffValue;
+        private EffectObject effect;
         public EffectBondage(Agent agent, bool isResist) : base(agent, isResist)
         {
         }
@@ -11,10 +15,13 @@
         {
             _buffValue = effectLevel;
             _owner.Stat.moveSpeed.AddModifier(-_buffValue);
+            effect = PoolingManager.Instance.Pop(PoolingType.PoiserHitVFX) as EffectObject;
+            effect.Initialize(_owner.transform.position);
         }
 
         protected override void UpdateState()
         {
+            effect.Initialize(_owner.transform.position);
         }
 
         protected override void UpdateStateBySecond()
@@ -23,6 +30,8 @@
 
         public override void Over()
         {
+            if(effect != null)
+                PoolingManager.Instance.Push(effect);
             _owner.Stat.moveSpeed.RemoveModifier(-_buffValue);
         }
     }
