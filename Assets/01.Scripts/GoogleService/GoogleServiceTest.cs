6a3d0b14ff28681d4d6e1c5a323a889404a09861
qazplm5602 <t01048187045@gmail.com>
2024-08-25 21:52:10 +0900

    feat: google rank add

--- Assets/01.Scripts/GoogleService/GoogleServiceTest.cs
+++ Assets/01.Scripts/GoogleService/GoogleServiceTest.cs
@@ -1,19 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoogleServiceTest : MonoBehaviour
 {
-public string Token;
+    [SerializeField] Button activeBtn;
+    [SerializeField] Button leaderboardBtn;
+
+    public string Token;
     public string Error;
 
     void Awake()
     {
+        activeBtn.onClick.AddListener(LoginGoogleService);    
+        leaderboardBtn.onClick.AddListener(OpenLeaderBoard);    
+    }
+
+    void LoginGoogleService() {
         //Initialize PlayGamesPlatform
+        print("LoginGoogleService");
+
         PlayGamesPlatform.Activate();
         LoginGooglePlayGames();
+    }
+
+    private void OpenLeaderBoard()
+    {
+        print("OpenLeaderBoard");
+        PlayGamesPlatform.Instance.ShowLeaderboardUI("CgkInoqooYweEAIQAg", (status) => {
+            print($"ShowLeaderboardUI status: {status.ToString()}");
+        });
     }
 
     public void LoginGooglePlayGames()
