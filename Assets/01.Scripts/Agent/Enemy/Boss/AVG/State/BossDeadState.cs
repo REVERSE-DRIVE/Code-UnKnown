88844ce1c9dc4580075b560cf72246cbb64ec9f2
leo82380 <tkdrb062@gmail.com>
2024-09-03 01:06:16 +0900

    feat: BossDeadState

---
+++ Assets/01.Scripts/Agent/Enemy/Boss/AVG/State/BossDeadState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace EnemyManage
+{
+    public class BossDeadState : BossAVGState
+    {
+        public BossDeadState(Enemy enemyBase, EnemyStateMachine<AVGStateEnum> stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
+        {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            _bossAVGBase.MovementCompo.StopImmediately();
+        }
+
+        public override void UpdateState()
+        {
+            base.UpdateState();
+            if (_endTriggerCalled)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            _bossAVGBase.isDead = true;
+            _bossAVGBase.ColliderCompo.enabled = false;
+            Object.Destroy(_bossAVGBase.gameObject);
+        }
+    }
+}
