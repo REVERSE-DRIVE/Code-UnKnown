6d98bc4ac81226b196efc79477fa73acdbdd910b
leo82380 <tkdrb062@gmail.com>
2024-09-06 10:29:51 +0900

    feat: Dynamic Joystick

--- Assets/01.Scripts/UI/GameScene/JoyStick.cs
+++ Assets/01.Scripts/UI/GameScene/JoyStick.cs
@@ -1,8 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
 
-public class JoyStick : MonoBehaviour
+public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+    [SerializeField] private Image _stickBG;
+    [SerializeField] private OnScreenStick _stick;
     
+    private RectTransform _stickBGRect;
+    private RectTransform _baseRect;
+    private Camera cam;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        _stickBGRect = _stickBG.GetComponent<RectTransform>();
+        _baseRect = GetComponent<RectTransform>();
+        cam = canvas.worldCamera;
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _stickBGRect.anchoredPosition = eventData.position - _baseRect.sizeDelta / 2;
+        _stickBG.gameObject.SetActive(true);
+        _stick.OnPointerDown(eventData);
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _stickBG.gameObject.SetActive(false);
+        _stick.OnPointerUp(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _stick.OnDrag(eventData);
+    }
 }
