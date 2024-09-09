b40e044c36e0443815a82b7d2f4d74cf1491ece6
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-09-09 22:54:20 +0900

    feat: Editor Resource Display

--- Assets/01.Scripts/Managers/ResourceManager.cs
+++ Assets/01.Scripts/Managers/ResourceManager.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ResourceManager : MonoSingleton<ResourceManager>
 {
+    public event Action<int> OnResourceChnaged;
     public int ResourceAmount { get; private set; } = 1000;
 
     public void AddResource(int amount)
     {
         ResourceAmount += amount;
+        OnResourceChnaged?.Invoke(ResourceAmount);
+
     }
     
     public void UseResource(int amount)
     {
         ResourceAmount -= amount;
+        OnResourceChnaged?.Invoke(ResourceAmount);
     }
 }
