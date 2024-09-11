7d91e66cb71c5bcd895a8d225ac73f0d45fecfa6
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-09-11 14:07:50 +0900

    feat: Enemy Die Detecter

--- Assets/01.Scripts/Tutorial/DoorObject.cs
+++ Assets/01.Scripts/Tutorial/DoorObject.cs
@@ -4,11 +4,18 @@ using UnityEngine.Events;
 public class DoorObject : MonoBehaviour
 {
     public UnityEvent OnDoorOpenEvent;
+    public UnityEvent OnDoorCloseEvent;
     
     public void OpenDoor()
     {
         OnDoorOpenEvent?.Invoke();
         gameObject.SetActive(false);    
+    }
+
+    public void CloseDoor()
+    {
+        OnDoorCloseEvent?.Invoke();
+        gameObject.SetActive(true);  
     }
     
 }
--- Assets/01.Scripts/Tutorial/TutorialManager.cs
+++ Assets/01.Scripts/Tutorial/TutorialManager.cs
@@ -1,8 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoSingleton<TutorialManager>
 {
+    [SerializeField] private Player _player;
+    [SerializeField] private TutorialFailurePanel _tutorialFailurePanel;
+
+    private void Start()
+    {
+        _player.HealthCompo.OnDieEvent.AddListener(HandlePlayerDie);
+    }
+
+    private void HandlePlayerDie()
+    {
+        
+        _tutorialFailurePanel.Open();
+    }
+
+    private IEnumerator PlayerDieCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
     
+    private void ExitScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+
 }
