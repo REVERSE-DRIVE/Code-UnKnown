f44b47ad627a240b71b68b4e4dd876d67f82df40
leo82380 <tkdrb062@gmail.com>
2024-09-26 01:34:02 +0900

    fix: Shokety Enemy Skill

--- Assets/01.Scripts/Agent/Enemy/EnemyScript/Elete/Shokety/ShoketyEnemy.cs
+++ Assets/01.Scripts/Agent/Enemy/EnemyScript/Elete/Shokety/ShoketyEnemy.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class ShoketyEnemy : EnemyBase
 {
     [field:SerializeField] public LineRenderer LineRendererCompo { get; private set; }
+    [field:SerializeField] public OverlapDamageCaster OverlapDamageCaster { get; private set; }
+    
     protected override void Awake()
     {
         base.Awake();
         StateMachine.AddState(EnemyStateEnum.Idle, new EnemyIdleState(this, StateMachine, "Idle"));
         StateMachine.AddState(EnemyStateEnum.Chase, new TargetFollowChaseState(this, StateMachine, "Chase", 3f));
         StateMachine.AddState(EnemyStateEnum.Attack, new ShoketyAttackState(this, StateMachine, "Attack"));
+        StateMachine.AddState(EnemyStateEnum.Skill, new ShoketySkillState(this, StateMachine, "Skill"));
         StateMachine.AddState(EnemyStateEnum.Dead, new EnemyDeadState(this, StateMachine, "Dead"));
     }
 }
