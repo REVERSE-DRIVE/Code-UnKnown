83a0f7a7426f1728c5f79a20f727fa823057909a
qazplm5602 <t01048187045@gmail.com>
2024-09-26 00:26:54 +0900

    feat: player revivial system

--- Assets/01.Scripts/UI/DeadPanel.cs
+++ Assets/01.Scripts/UI/DeadPanel.cs
@@ -9,6 +9,7 @@ using TMPro;
 public class DeadPanel : MonoBehaviour, IWindowPanel
 {
     [SerializeField] private Button _backButton;
+    [SerializeField] private Button _RevivalButton;
     [SerializeField] private TextMeshProUGUI _titleText;
     [SerializeField] private TextMeshProUGUI _liveTimeText;
     [SerializeField] private TextMeshProUGUI _resourceText;
@@ -16,6 +17,7 @@ public class DeadPanel : MonoBehaviour, IWindowPanel
     
     private CanvasGroup _canvasGroup;
     private bool _isShow;
+    bool _revivalActive = true;
 
     private void Awake()
     {
@@ -26,6 +28,17 @@ public class DeadPanel : MonoBehaviour, IWindowPanel
     {
         _backButton.onClick.AddListener(HandleLobbyMove);
         _backButton.gameObject.SetActive(false);
+
+        _RevivalButton.onClick.AddListener(HandleRevivalPlayer);
+        _RevivalButton.gameObject.SetActive(false);
+    }
+
+    private void HandleRevivalPlayer()
+    {
+        GoogleRewardManager.Instance.Show(() => {
+            Close();
+            PlayerManager.Instance.Revival();
+        });
     }
 
     private void HandleLobbyMove()
@@ -40,11 +53,19 @@ public class DeadPanel : MonoBehaviour, IWindowPanel
     {
         SetVisible(true);
         SetUI(InGameCounter.Instance.TimeCounter, 10, 5);
+
+        // 부활 이미 하면 비활함
+        if (!PlayerManager.Instance.IsFirstSpawn)
+            RevivalBtnHide();
     }
     
     public void SetTitle(string title)
     {
         _titleText.text = title;
+    }
+
+    public void RevivalBtnHide() {
+        _revivalActive = false;
     }
 
     public void SetUI(float time, int resourceAmount, int partAmount)
@@ -69,11 +90,19 @@ public class DeadPanel : MonoBehaviour, IWindowPanel
         
         yield return new WaitForSecondsRealtime (1f);
         _backButton.gameObject.SetActive(true);
+        
+        if (_revivalActive)
+           _RevivalButton.gameObject.SetActive(true);
     }
 
     public void Close()
     {
-        
+        _isShow = false;
+        Time.timeScale = 1;
+        _backButton.gameObject.SetActive(false);
+        _RevivalButton.gameObject.SetActive(false);
+        SetVisible(false);
+
     }
     
     private void SetVisible(bool visible)
